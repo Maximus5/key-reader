0960e4f8a07d96b5f2c2f642718a7c900de8465c
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-07-28 12:40:07 +0300

    Let key-curses.cpp read and print mouse events

--- key-curses.cpp
+++ key-curses.cpp
@@ -36,16 +36,31 @@ int main()
     initscr();
     raw();
     keypad(stdscr, TRUE);
+    mousemask(ALL_MOUSE_EVENTS, NULL);
+    //mouseinterval(0);
     noecho();
     printf("Press <Esc> twice to exit\r\n");
     refresh();
     while (esc <= 1)
     {
         ch = getch();
-        if (ch == ERR) break;
+        if (ch == ERR)
+        {
+            printf("  getch fails, code=%i\r\n", (int)ch);
+            break;
+        }
         printf("<x%02x:%c> %s\r\n",
             ch, (ch>32 && ch<=127 && ch!=10 && ch!=13)?ch:' ',
             keyname(ch));
+        if (ch == KEY_MOUSE)
+        {
+            MEVENT event;
+            int mrc = getmouse(&event);
+            if (mrc == OK)
+                printf("  bstate=x%02X x=%i y=%i\r\n", (unsigned)event.bstate, event.x, event.y);
+            else
+                printf("  getmouse fails, code=%i\r\n", mrc);
+        }
         if (ch == 0x1B) esc++; else esc = 0;
         refresh();
     }
